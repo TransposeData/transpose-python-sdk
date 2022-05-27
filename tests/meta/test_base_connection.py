357f79c7c2e2e3df194949b1b467fd1a8896826c
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2022-05-27 19:16:41 -0400

    Hopefully dotenv works now

--- tests/meta/test_base_connection.py
+++ tests/meta/test_base_connection.py
@@ -1,11 +1,8 @@
-from transpose import Transpose
+from transpose import Transpose, api_key
 from transpose.src.util.errors import TransposeInvalidAPIKey
-
-from transpose.src.util.io import read_json_value_at_path
 
 def test_valid_connection():
     try:
-        api_key = read_json_value_at_path('./keys/api.json', 'api_key')
         api = Transpose(api_key)
         assert isinstance(api, (Transpose,))
     except Exception:
@@ -13,7 +10,6 @@ def test_valid_connection():
 
 def test_invalid_api_key():
     try:
-        api_key = read_json_value_at_path('./keys/api.json', 'api_key')
         api = Transpose('some_invalid_api_key')
         assert False
     except TransposeInvalidAPIKey:
--- tests/meta/test_ens_ens_base_class.py
+++ tests/meta/test_ens_ens_base_class.py
@@ -1,10 +1,7 @@
-from transpose import Transpose
-
-from transpose.src.util.io import read_json_value_at_path
+from transpose import Transpose, api_key
 
 def test_ens_instantiation():
     try:
-        api_key = read_json_value_at_path('./keys/api.json', 'api_key')
         api = Transpose(api_key)
         
         print(api.ENS)
