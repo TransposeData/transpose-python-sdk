2d5522696099d912b2402719d4c4b56fcf504768
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2022-06-08 14:43:06 -0400

    pagination helper

--- transpose/src/base.py
+++ transpose/src/base.py
@@ -1,3 +1,4 @@
+import time
 import requests
 
 from ..src.util.models import *
@@ -28,6 +29,21 @@ class Transpose:
     
     def next(self) -> str:
         return self.perform_authorized_request(self._next_class_name, self._next)
+    
+    # this can be renamed later. Pagination helper function to get many 
+    def bulk_request(self, endpoint_response: List, requests_per_second: int=None, results_to_fetch: int=9999999999999) -> List:
+        api_response_data = endpoint_response
+    
+        while (len(api_response_data) < results_to_fetch) or self._next is None:
+            
+            api_response_data += self.next()
+            
+            # if the user specified a requests per second, sleep for the appropriate amount of time
+            # has a 1% buffer.
+            if requests_per_second is not None:
+                time.sleep(1.01 / requests_per_second)
+        
+        return api_response_data
     
     # the base function for performing authorized requests to the Transpose API suite
     def perform_authorized_request(self, model: type, endpoint: str, api_key: str=None) -> str:
