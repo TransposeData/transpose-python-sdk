1408e1fdff7f736288647496f516c7660b3a53c6
Mihai <mihai@element.fi>
2022-08-05 20:10:27 -0400

    fix: call block_number property of BlockObject

--- demo/gas.py
+++ demo/gas.py
@@ -6,4 +6,4 @@ api = Transpose('API_KEY')
 
 blocks = api.block.blocks_by_number(order='desc', limit=500)
 fig, ax = plt.subplots()
-ax.plot([x.number for x in blocks], [x.gas_used for x in blocks])
+ax.plot([x.block_number for x in blocks], [x.gas_used for x in blocks])
