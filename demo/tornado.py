1d4962b32e32bbcdd8ec456f5c15c54119bde7dc
Jonathan Becker <jonathan@jbecker.dev>
2022-08-16 17:07:43 -0400

    fix: add deposit demo back

--- demo/tornado.py
+++ demo/tornado.py
@@ -7,20 +7,30 @@ from transpose.extras import Plot
 from transpose import Transpose
 import multiprocessing.pool as mpp
 
-api = Transpose('API_KEY', True)
+api = Transpose('h5TQ5m4u7W1C4Pmq5rytL7hJMWsOHPjM12OUSyjh', True)
 
 # get all tornado cash deposits since 2022
+tornado_deposit_series = {"date": [], "usage": []}
 tornado_withdrawl_series = {"date": [], "usage": []}
+tornado_deposits = api.bulk_request(api.block.logs_by_date(
+    emitted_after=datetime(2022, 1, 1, tzinfo=timezone.utc),
+    event_signature='0xa945e51eec50ab98c161376f0db4cf2aeba3ec92755fe2fcd388bdbbb80ff196', limit=500))
 tornado_withdrawls = api.bulk_request(api.block.logs_by_date(
     emitted_after=datetime(2022, 1, 1, tzinfo=timezone.utc),
     event_signature='0xe9e508bad6d4c3227e881ca19068f099da81b5164dd6d62b2eaf1e8bc6c34931', limit=500))
-current_date = "" 
+current_date = ""
 
 # pull all addresses from the tornado withdrawls
 recieving_addresses = []
 
 # iterate through the tornado deposits and withdrawals and add them to the tornado series, grouping by date
-
+for log in tornado_deposits:
+    if datetime.fromisoformat(log.timestamp.split("T")[0]) == current_date:
+        tornado_deposit_series['usage'][-1] += 1
+    else:
+        current_date = datetime.fromisoformat(log.timestamp.split("T")[0])
+        tornado_deposit_series['date'].append(current_date)
+        tornado_deposit_series['usage'].append(1)
 for log in tornado_withdrawls:
     recieving_addresses.append("0x{}".format(log.data[26:66]))
 
@@ -33,7 +43,18 @@ for log in tornado_withdrawls:
 
 # plot the tornado series
 chart = Plot(title="Tornado.cash Activity Since 2022")
-
+chart.add_data(
+    name="Deposits",
+    data={
+        "x": tornado_deposit_series['date'],
+        "y": tornado_deposit_series['usage'],
+        "x_axis": "Time",
+        "y_axis": "Deposits",
+    },
+    type="line",
+    shape="spline",
+    smoothing=4,
+)
 chart.add_data(
     name="Withdrawls",
     data={
