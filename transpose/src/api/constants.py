1421da886e0adc3abee6cab88cedc7100c2e0487
jon-becker <jonathan@jbecker.dev>
2022-08-31 16:00:08 -0400

    fix: rename endpoint

--- transpose/src/api/constants.py
+++ transpose/src/api/constants.py
@@ -3,7 +3,7 @@ ENS_API_ENDPOINTS = {
 
     # ENS Record Endpoints
     'records_by_owner':                       'https://api.transpose.io/v0/ens/ens-records-by-owner',
-    'records_by_account':                     'https://api.transpose.io/v0/ens/ens-records-by-account',
+    'records_by_account':                     'https://api.transpose.io/v0/ens/ens-records-by-resolved-account',
     'records_by_ens_name':                    'https://api.transpose.io/v0/ens/ens-records-by-name',
     'records_by_ens_token_id':                'https://api.transpose.io/v0/ens/ens-records-by-token-id',
     'records_by_date':                        'https://api.transpose.io/v0/ens/ens-records-by-date',
