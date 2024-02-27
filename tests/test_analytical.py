1510523a9cb7d1225ad286899a9fb6997e947fb5
Patrick Croke <86120203+tannishmango@users.noreply.github.com>
2024-02-27 08:58:09 -0700

    Create test_analytical.py

---
+++ tests/test_analytical.py
@@ -0,0 +1,59 @@
+from transpose import Transpose, api_key
+from transpose.src.base import TransposeBadRequest
+
+
+def test_basic():
+    try:
+        api = Transpose(api_key)
+
+        response = api.analytical.query("SELECT * FROM cross_chain.transaction_flows LIMIT 1;")
+
+        assert response['stats']['count'] == 1
+        assert len(response['results']) == 1
+
+    except Exception:
+        assert False
+
+
+def test_batch():
+    try:
+        api = Transpose(api_key)
+
+        response = api.analytical.query("SELECT * FROM cross_chain.transaction_flows LIMIT 10;")
+
+        assert response['stats']['count'] == 10
+        assert len(response['results']) == 10
+
+    except Exception:
+        assert False
+
+
+def test_invalid_query():
+    try:
+        api = Transpose(api_key)
+
+        response = api.analytical.query("SELECT * FROM cross_chan.transaction_flows LIMIT 1;")
+
+        # assert throws
+        assert False
+
+    except TransposeBadRequest:
+        assert True
+
+    except Exception:
+        assert False
+
+
+def test_query_parameters():
+    try:
+        api = Transpose(api_key)
+
+        query = "SELECT * FROM cross_chain.transaction_flows WHERE transaction_hash = '{{transaction_hash}}';"
+        parameters = {'transaction_hash': '0xeda18199abfc878196798af337c15beba1966b520cc6f68f1014406dfeb23552'}
+        response = api.analytical.query(sql_query=query, parameters=parameters)
+
+        assert response['stats']['count'] == 1
+        assert len(response['results']) == 1
+
+    except Exception:
+        assert False
